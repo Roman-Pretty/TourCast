06359e636fc2da9c533aa9497d7ca27e5ddb457f
ElenaiDev <ec22761@qmul.ac.uk>
2024-02-29 22:29:39 +0000

    Add menu icons to navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './css/App.css';
 import React from 'react';
 import Weather from './Weather';
+import {ReactComponent as MenuIcon} from './img/menu_icons/menu.svg';
+import {ReactComponent as RunIcon} from './img/menu_icons/directions_run.svg';
+import {ReactComponent as PollutionIcon} from './img/menu_icons/Pollution 1.svg';
+import {ReactComponent as WindIcon} from './img/menu_icons/Wind 1.svg';
+import {ReactComponent as CloudIcon} from './img/menu_icons/cloud.svg';
 
 function Nav() {
   return (
     <nav>
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/weather">Weather</a></li>
+      <li><a href="/"><MenuIcon /></a></li>
+      <li><a href="/"><RunIcon /></a></li>
+      <li><a href="/"><CloudIcon /></a></li>
+        <li><a href="/"><WindIcon /></a></li>
+        <li><a href="/weather"><PollutionIcon /></a></li>
       </ul>
     </nav>
   );
