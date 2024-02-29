36f338862d867e07b59194d520f65fae4f3c928d
ElenaiDev <ec22761@qmul.ac.uk>
2024-02-29 22:10:51 +0000

    Started work on header component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './css/App.css';
 import React from 'react';
 import Weather from './Weather';
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/weather">Weather</a></li>
+      </ul>
+    </nav>
+  );
+
+}
+
+function Header() {
+  return (
+    <header>
+      <Nav />
+    </header>
+  );
+
+}
+
 const App = () => {
   return (
     <div>
+      <Header />
       <h1>Weather Forecast App</h1>
       <Weather />
     </div>
