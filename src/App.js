65962a7e5762f00483cd6496e40a737888d2d110
ec22761 <ec22761@qmul.ac.uk>
2024-03-01 17:05:57 +0000

    FInished Header Skeleton

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
 import './css/App.css';
 import React from 'react';
 import Weather from './Weather';
-import {ReactComponent as MenuIcon} from './img/menu_icons/menu.svg';
-import {ReactComponent as RunIcon} from './img/menu_icons/directions_run.svg';
-import {ReactComponent as PollutionIcon} from './img/menu_icons/Pollution 1.svg';
-import {ReactComponent as WindIcon} from './img/menu_icons/Wind 1.svg';
-import {ReactComponent as CloudIcon} from './img/menu_icons/cloud.svg';
+import { ReactComponent as MenuIcon } from './img/menu_icons/menu.svg';
+import { ReactComponent as RunIcon } from './img/menu_icons/directions_run.svg';
+import { ReactComponent as PollutionIcon } from './img/menu_icons/Pollution 1.svg';
+import { ReactComponent as WindIcon } from './img/menu_icons/Wind 1.svg';
+import { ReactComponent as CloudIcon } from './img/menu_icons/cloud.svg';
 
 function Nav() {
   return (
     <nav>
       <ul>
-      <li><a href="/"><MenuIcon /></a></li>
-      <li><a href="/"><RunIcon /></a></li>
-      <li><a href="/"><CloudIcon /></a></li>
+        <li><a href="/"><MenuIcon /></a></li>
+        <li><a href="/"><RunIcon /></a></li>
+        <li><a href="/"><CloudIcon /></a></li>
         <li><a href="/"><WindIcon /></a></li>
         <li><a href="/weather"><PollutionIcon /></a></li>
       </ul>
     </nav>
   );
+}
 
+function Title() {
+  return (
+    <div>
+      <h1>London</h1>
+    </div>
+  );
 }
 
 function Header() {
   return (
     <header>
       <Nav />
+      <Title />
+      <div id="space"></div>
     </header>
   );
 
