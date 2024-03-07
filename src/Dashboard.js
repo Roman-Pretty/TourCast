6afe4361356defbeca9ce05c14fcc2bd82c4145f
ElenaiDev <ec22761@qmul.ac.uk>
2024-03-07 21:29:22 +0000

    Add weekly forecast to Dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,6 +2,10 @@ import './css/App.css';
 import React from 'react';
 import { ReactComponent as Cloudy } from './img/weather/cloudy.svg';
 import { ReactComponent as Background } from './img/bg.svg';
+import { ReactComponent as RainyWeek } from './img/weather/week/Rainy.svg';
+import { ReactComponent as SunWeek } from './img/weather/week/Sun.svg';
+import { ReactComponent as StormyWeek } from './img/weather/week/Stormy.svg';
+import { ReactComponent as CloudyWeek } from './img/weather/week/Cloudy.svg';
 
 function TodayWeather() {
   return (
@@ -18,10 +22,49 @@ function TodayWeather() {
   );
 }
 
+function WeekItem({day, weather, temp}) {
+  let weatherComponent = <CloudyWeek />;
+
+  if (weather === "Rainy") {
+    weatherComponent = <RainyWeek />;
+  } else if (weather === "Sunny") {
+    weatherComponent = <SunWeek />;
+  } else if (weather === "Stormy") {
+    weatherComponent = <StormyWeek />;
+  } else if (weather === "Cloudy") {
+    weatherComponent = <CloudyWeek />;
+  }
+
+  return (
+    <div class="week-item">
+      <div class="week-item-divider"></div>
+      <div class="week-item-content">
+        <div class="week-item-text">
+          <h3>{day}</h3>
+          <p>{weather}<br/>{temp}°</p>
+        </div>
+        {weatherComponent}
+      </div>
+    </div>
+  );
+}
+
+function Forecast() {
+  return (
+    <div id="forecast">
+      <WeekItem day="Tom" weather="Rainy" temp="4"/>
+      <WeekItem day="Wed" weather="Cloudy" temp="8"/>
+      <WeekItem day="Thu" weather="Sunny" temp="19"/>
+      <WeekItem day="Fri" weather="Stormy" temp="7"/>
+    </div>
+  );
+}
+
 const Dashboard = () => {
     return (
       <div id="dashboard">
         <TodayWeather />
+        <Forecast />
         <Background />
       </div>
       
