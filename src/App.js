f57b237f1a6a9339ffa3dafb04b5c6cf7674c0e8
Roman Pretty <ec22761@qmul.ac.uk>
2024-03-13 17:37:13 +0000

    Added Mobile.js.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, {useState} from 'react';
 import Header from './Header';
 import Dashboard from './Dashboard';
 import Activities from './Activities';
+import useWindowDimensions from './WindowDimensions';
+import Mobile from './Mobile';
 
 export const DayContext = React.createContext();
 export const WeekContext = React.createContext();
@@ -13,10 +15,13 @@ const App = () => {
   const [page, setPage] = useState('dashboard-page');
   const [weatherData, setWeatherData] = useState(null);
   const [weekData, setWeekData] = useState(null);
+  const { height, width } = useWindowDimensions();
 
   let pageComponent = null;
 
-  if (page == 'dashboard-page') {
+  if (width <= 768) {
+    pageComponent = <Mobile />;
+  } else if (page == 'dashboard-page') {
     pageComponent = <Dashboard />;
   } else if (page == 'activities-page') {
     pageComponent = <Activities />;
@@ -26,7 +31,7 @@ const App = () => {
     <PageContext.Provider value={[page, setPage]}>
       <DayContext.Provider value={[weatherData, setWeatherData]}>
         <WeekContext.Provider value={[weekData, setWeekData]}>
-          <Header />
+          {width > 768 ? <Header /> : null}
           {pageComponent}
         </WeekContext.Provider>
       </DayContext.Provider>
---
+++ src/Mobile.js
@@ -0,0 +1,9 @@
+import './css/App.css';
+import React, {useState} from 'react';
+
+const Mobile = () => {
+  return (
+    <div></div>
+  );
+};
+export default Mobile;
