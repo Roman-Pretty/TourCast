8f82a5cd845c6290f53e72b9359aeae44fd1e885
Roman Pretty <ec22761@qmul.ac.uk>
2024-03-13 18:55:00 +0000

    Added loading animation for API access.

--- src/Header.js
+++ src/Header.js
@@ -38,7 +38,10 @@ function Title() {
   const [city, setCity] = useState('London');
   const [weatherData, setWeatherData] = useContext(DayContext);
   const [weekData, setWeekData] = useContext(WeekContext);
+  const [isLoading, setIsLoading] = useState('loading');
+
   const fetchData = async () => {
+    setIsLoading('loading');
     try {
       const response = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=717fad74b9058562c326494dcbd56f58`
@@ -56,6 +59,7 @@ function Title() {
     } catch (error) {
       console.error(error);
     }
+    setIsLoading(null);
   };
   useEffect(() => {
     fetchData();
@@ -73,7 +77,7 @@ function Title() {
       {/* <select id="dropdown" value={city} onChange={handleSubmit}></select> */}
       <fieldset>
         <input type="text" value={city} onChange={handleInputChange}></input>
-        <button type='submit'>
+        <button type='submit' className={isLoading}>
           <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 33 33" fill="none">
             <path d="M32 16.5C32 25.0604 25.0604 32 16.5 32C7.93959 32 1 25.0604 1 16.5C1 7.93959 7.93959 1 16.5 1C25.0604 1 32 7.93959 32 16.5Z" fill="#A1B4D7" />
             <path fill-rule="evenodd" clip-rule="evenodd" d="M15.6667 21C12.7211 21 10.3333 18.6122 10.3333 15.6667C10.3333 12.7211 12.7211 10.3333 15.6667 10.3333C18.6122 10.3333 21 12.7211 21 15.6667C21 16.8991 20.5819 18.034 19.8799 18.9371L23.4714 22.5286L22.5286 23.4714L18.9371 19.8799C18.034 20.5819 16.8991 21 15.6667 21ZM19.6667 15.6667C19.6667 17.8758 17.8758 19.6667 15.6667 19.6667C13.4575 19.6667 11.6667 17.8758 11.6667 15.6667C11.6667 13.4575 13.4575 11.6667 15.6667 11.6667C17.8758 11.6667 19.6667 13.4575 19.6667 15.6667Z" fill="#444444" />
