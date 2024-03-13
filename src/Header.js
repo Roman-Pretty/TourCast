ff213a359f3412de7aece713cf637d183dfae6ff
Roman Pretty <ec22761@qmul.ac.uk>
2024-03-13 14:03:34 +0000

    Switched to search bar

--- src/Header.js
+++ src/Header.js
@@ -1,131 +1,99 @@
-import React, {useState, useContext, useEffect} from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { ReactComponent as MenuIcon } from './img/menu_icons/menu.svg';
 import { ReactComponent as RunIcon } from './img/menu_icons/directions_run.svg';
 import { ReactComponent as PollutionIcon } from './img/menu_icons/Pollution 1.svg';
 import { ReactComponent as WindIcon } from './img/menu_icons/Wind 1.svg';
 import { ReactComponent as CloudIcon } from './img/menu_icons/cloud.svg';
-import { ReactComponent as Down } from './img/menu_icons/chevron-bottom.svg';
 import { DayContext, WeekContext, PageContext } from './App';
 import axios from 'axios';
 
-function Tooltip({text}) {
-    return (
-      <p class="tooltip">
-        {text}
-      </p>
-    );
+function Tooltip({ text }) {
+  return (
+    <p class="tooltip">
+      {text}
+    </p>
+  );
 }
 
 function Nav() {
 
   const [page, setPage] = useContext(PageContext);
 
-    return (
-      <nav>
-        <div id="selected" class={page}></div>
-        <ul>
-          <li><a onClick={() => setPage("dashboard-page")}><MenuIcon /></a><Tooltip text="Dashboard" /></li>
-          <li><a onClick={() => setPage("activities-page")}><RunIcon /></a><Tooltip text="Activities" /></li>
-          <li><a onClick={() => setPage("weather-page")}><CloudIcon /></a><Tooltip text="Weather" /></li>
-          <li><a onClick={() => setPage("wind-page")}><WindIcon /></a><Tooltip text="Wind" /></li>
-          <li><a onClick={() => setPage("pollution-page")}><PollutionIcon /></a><Tooltip text="Pollution" /></li>
-        </ul>
-      </nav>
-    );
-  }
-  
-  function Title() {
+  return (
+    <nav>
+      <div id="selected" class={page}></div>
+      <ul>
+        <li><a onClick={() => setPage("dashboard-page")}><MenuIcon /></a><Tooltip text="Dashboard" /></li>
+        <li><a onClick={() => setPage("activities-page")}><RunIcon /></a><Tooltip text="Activities" /></li>
+        <li><a onClick={() => setPage("weather-page")}><CloudIcon /></a><Tooltip text="Weather" /></li>
+        <li><a onClick={() => setPage("wind-page")}><WindIcon /></a><Tooltip text="Wind" /></li>
+        <li><a onClick={() => setPage("pollution-page")}><PollutionIcon /></a><Tooltip text="Pollution" /></li>
+      </ul>
+    </nav>
+  );
+}
 
-    const [city, setCity] = useState('');
-    const [weatherData, setWeatherData] = useContext(DayContext);
-    const [weekData, setWeekData] = useContext(WeekContext);
-    const fetchData = async (selectedCity) => {
-      try {
-        const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&units=metric&appid=717fad74b9058562c326494dcbd56f58`
-        );
-        setWeatherData(response.data);
-      } catch (error) {
-        console.error(error);
-      }
+function Title() {
 
-      try {
-        const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/forecast/?q=${selectedCity}&units=metric&appid=717fad74b9058562c326494dcbd56f58`
-        );
-        setWeekData(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    useEffect(() => {
-      if (city) {
-        fetchData(city);
-      } else {
-        //TODO: Save default city in local storage
-        fetchData('London');
-      }
-    }, [city]);
-    const handleSubmit = (e) => {
-      const selectedCity = e.target.value;
-      setCity(selectedCity);
-      e.preventDefault();
-    };
-
-    return (
-      <form id="form">
-      <select id="dropdown" value={city} onChange={handleSubmit}>
-        <optgroup label="UK">
-          <option value="london">London</option>
-          <option value="birmingham">Birmingham</option>
-          <option value="bristol">Bristol</option>
-          <option value="edinburgh">Edinburgh</option>
-          <option value="glasgow">Glasgow</option>
-          <option value="leeds">Leeds</option>
-          <option value="liverpool">Liverpool</option>
-          <option value="manchester">Manchester</option>
-          <option value="newcastle">Newcastle</option>
-          <option value="nottingham">Nottingham</option>
-          <option value="norwich">Norwich</option>
-        </optgroup>
-        <optgroup label="USA">
-          <option value="atlanta">Atlanta</option>
-          <option value="austin">Austin</option>
-          <option value="boston">Boston</option>
-          <option value="chicago">Chicago</option>
-          <option value="dallas">Dallas</option>
-          <option value="denver">Denver</option>
-          <option value="houston">Houston</option>
-          <option value="las vegas">Las Vegas</option>
-          <option value="los angeles">Los Angeles</option>
-          <option value="miami">Miami</option>
-          <option value="new york">New York</option>
-          <option value="orlando">Orlando</option>
-          <option value="philadelphia">Philadelphia</option>
-          <option value="phoenix">Phoenix</option>
-          <option value="san antonio">San Antonio</option>
-          <option value="san diego">San Diego</option>
-          <option value="san francisco">San Francisco</option>
-          <option value="san jose">San Jose</option>
-          <option value="seattle">Seattle</option>
-          <option value="washington">Washington</option>
-        </optgroup>
-        <optgroup label="Other Countries">
-          <option value="mexico city">Mexico City</option>
-          <option value="rome">Rome</option>
-        </optgroup>
-      </select>
-      </form>
-    );
+  const [city, setCity] = useState('London');
+  const [weatherData, setWeatherData] = useContext(DayContext);
+  const [weekData, setWeekData] = useContext(WeekContext);
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=717fad74b9058562c326494dcbd56f58`
+      );
+      setWeatherData(response.data);
+      console.log(response.data); //You can see all the weather data in console log
+    } catch (error) {
+      console.error(error);
     }
-  
-  const Header = () => {
-    return (
-      <header>
-        <Nav />
-        <Title />
-        <div id="space"></div>
-      </header>
-    );
+
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast/?q=${city}&units=metric&appid=717fad74b9058562c326494dcbd56f58`
+      );
+      setWeekData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const handleInputChange = (e) => {
+    setCity(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchData();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {/* <select id="dropdown" value={city} onChange={handleSubmit}></select> */}
+      <fieldset>
+        <input type="text" value={city} onChange={handleInputChange}></input>
+        <button type='submit'>
+          <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 33 33" fill="none">
+            <path d="M32 16.5C32 25.0604 25.0604 32 16.5 32C7.93959 32 1 25.0604 1 16.5C1 7.93959 7.93959 1 16.5 1C25.0604 1 32 7.93959 32 16.5Z" fill="#A1B4D7" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M15.6667 21C12.7211 21 10.3333 18.6122 10.3333 15.6667C10.3333 12.7211 12.7211 10.3333 15.6667 10.3333C18.6122 10.3333 21 12.7211 21 15.6667C21 16.8991 20.5819 18.034 19.8799 18.9371L23.4714 22.5286L22.5286 23.4714L18.9371 19.8799C18.034 20.5819 16.8991 21 15.6667 21ZM19.6667 15.6667C19.6667 17.8758 17.8758 19.6667 15.6667 19.6667C13.4575 19.6667 11.6667 17.8758 11.6667 15.6667C11.6667 13.4575 13.4575 11.6667 15.6667 11.6667C17.8758 11.6667 19.6667 13.4575 19.6667 15.6667Z" fill="#444444" />
+            <path d="M32 16.5C32 25.0604 25.0604 32 16.5 32C7.93959 32 1 25.0604 1 16.5C1 7.93959 7.93959 1 16.5 1C25.0604 1 32 7.93959 32 16.5Z" stroke="#444444" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M15.6667 21C12.7211 21 10.3333 18.6122 10.3333 15.6667C10.3333 12.7211 12.7211 10.3333 15.6667 10.3333C18.6122 10.3333 21 12.7211 21 15.6667C21 16.8991 20.5819 18.034 19.8799 18.9371L23.4714 22.5286L22.5286 23.4714L18.9371 19.8799C18.034 20.5819 16.8991 21 15.6667 21ZM19.6667 15.6667C19.6667 17.8758 17.8758 19.6667 15.6667 19.6667C13.4575 19.6667 11.6667 17.8758 11.6667 15.6667C11.6667 13.4575 13.4575 11.6667 15.6667 11.6667C17.8758 11.6667 19.6667 13.4575 19.6667 15.6667Z" stroke="#444444" />
+          </svg>
+        </button>
+      </fieldset>
+    </form>
+  );
+}
+
+const Header = () => {
+  return (
+    <header>
+      <Nav />
+      <Title />
+      <div id="space"></div>
+    </header>
+  );
 };
 export default Header;
