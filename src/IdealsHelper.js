5e16273e833593c14508147edb2149f7b677effe
Roman Pretty <ec22761@qmul.ac.uk>
2024-03-13 17:11:25 +0000

    Updated IdealsHelper.

--- src/IdealsHelper.js
+++ src/IdealsHelper.js
@@ -2,26 +2,26 @@ function getIdeals(temperature, weather, visibility, windSpeed, humidity) {
     let ideals = ["Ideal","Ideal","Ideal","Ideal","Ideal","Ideal","Ideal"];
   
     // Running
-    if (visibility < 5000 || temperature < 5 || temperature > 30 || weather == "Rain" || windSpeed > 5.5|| humidity < 20 || humidity > 80) {
+    if (visibility < 5000 || temperature < 0 || temperature > 30 || weather == "Rain" || windSpeed > 10) {
       ideals[0] = "Poor"
     }
-    if (visibility < 2000 || temperature < -5 || temperature > 35 || weather == "Thunder" || weather == "Storm" || windSpeed > 8.3|| humidity < 10 || humidity > 90) {
+    if (visibility < 2000 || temperature < -5 || temperature > 35 || weather == "Thunder" || windSpeed > 20) {
       ideals[0] = "Warning"
     }
   
     // Camping
-    if (visibility < 5000 || temperature < 10 || temperature > 25 || weather == "Rain" || windSpeed > 5.5|| humidity < 40 || humidity > 70) {
+    if (visibility < 5000 || temperature < 10 || temperature > 30 || weather == "Rain" || windSpeed > 13) {
       ideals[1] = "Poor"
     }
-    if (visibility < 2000 || temperature < -5 || temperature > 35 || weather == "Thunder" || weather == "Storm" || windSpeed > 8.3|| humidity < 20 || humidity > 80) {
+    if (visibility < 2000 || temperature < -5 || temperature > 35 || weather == "Thunder" || windSpeed > 20) {
       ideals[1] = "Warning"
     }
     
     // Fishing
-    if (visibility < 5000 || temperature < 10 || temperature > 30 || windSpeed > 5.5) {
+    if (visibility < 5000 || temperature < 5 || temperature > 30 || windSpeed > 5.5) {
       ideals[2] = "Poor"
     }
-    if (visibility < 2000 || temperature < -5 || temperature > 35 || weather == "Thunder" || weather == "Storm" || windSpeed > 8.3|| humidity < 15 || humidity > 80) {
+    if (visibility < 2000 || temperature < -5 || temperature > 35 || weather == "Thunder" || windSpeed > 8.3) {
       ideals[2] = "Warning"
     }
   
@@ -29,12 +29,12 @@ function getIdeals(temperature, weather, visibility, windSpeed, humidity) {
     if (visibility < 4000 || temperature < 0 || temperature > 30 || weather == "Rain" || windSpeed > 5.5) {
       ideals[3] = "Poor"
     }
-    if (visibility < 2000 || temperature < -5 || temperature > 35 || weather == "Thunder" || weather == "Storm" || windSpeed > 8.3) {
+    if (visibility < 2000 || temperature < -10 || temperature > 35 || weather == "Thunder" || windSpeed > 8.3) {
       ideals[3] = "Warning"
     }
   
     // Watersports
-    if (visibility < 5000 || temperature < 18 || temperature > 25 ||  windSpeed > 5.14444) {
+    if (visibility < 5000 || temperature < 10 || temperature > 25 ||  windSpeed > 5.14444) {
       ideals[4] = "Poor"
     }
     if (visibility < 2000 || temperature < 5 || temperature > 35 || weather == "Thunder" || weather == "Storm" || windSpeed > 12) {
@@ -42,13 +42,13 @@ function getIdeals(temperature, weather, visibility, windSpeed, humidity) {
     }
   
     // Skiing
-    if (visibility < 40 || temperature < -6|| windSpeed > 7) {
+    if (visibility < 40 || temperature < -10|| windSpeed > 8) {
       ideals[5] = "Poor"
     }
-    if (visibility < 20 || temperature < -10 || weather == "Thunder" || weather == "Storm" || windSpeed > 13) {
+    if (visibility < 20 || temperature < -14 || weather == "Thunder" || windSpeed > 14) {
       ideals[5] = "Warning"
     }
-    if (temperature >= 0) {
+    if (temperature > 0) {
       ideals[5] = "Unavailable"
     }
   
