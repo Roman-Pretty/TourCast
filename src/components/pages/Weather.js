e98719b8e56c7e0270c4ab894eb810e6a3ad7ab1
ec22761 <ec22761@qmul.ac.uk>
2024-03-17 12:25:52 +0000

    Removed Rahi's JS page and implemented replacement as well as missing CSS.

--- src/components/pages/Weather.js
+++ src/components/pages/Weather.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { WeekContext } from '../../App';
+import { DayContext, WeekContext } from '../../App';
 
 import { ReactComponent as Background } from '../../img/bg-blur.svg';
 import { ReactComponent as Cloudy } from '../../img/weather/cloudy.svg';
@@ -7,90 +7,92 @@ import { ReactComponent as Rainy } from '../../img/weather/rainy.svg';
 import { ReactComponent as Sunny } from '../../img/weather/sunny.svg';
 import { ReactComponent as Stormy } from '../../img/weather/stormy.svg';
 
-function Card({ day, weather, temp, rain, humidity, windSpeed, cloudCover }) {
-  let weatherIcon;
-  switch (weather) {
-    case 'Rain':
-      weatherIcon = <Rainy />;
-      break;
-    case 'Clear':
-      weatherIcon = <Sunny />;
-      break;
-    case 'Storm':
-      weatherIcon = <Stormy />;
-      break;
-    case 'Clouds':
-      weatherIcon = <Cloudy />;
-      break;
-    default:
-      weatherIcon = <Cloudy />;
+function WeatherItem({ day, weather, temperature, feelsLike, humidity, wind, pressure, icon }) {
+
+  let weatherComponent = <Cloudy />;
+
+  if (icon === "Rain") {
+    weatherComponent = <Rainy />;
+  } else if (icon === "Clear") {
+    weatherComponent = <Sunny />;
+  } else if (icon === "Storm") {
+    weatherComponent = <Stormy />;
+  } else if (icon === "Clouds") {
+    weatherComponent = <Cloudy />;
   }
 
   return (
-    <div className="card">
-      <div className="card-date">
-        <p>{day}</p>
+    <div className="weather-item">
+      <div className='day'>
+        <h1>{day}</h1>
+        {weatherComponent}
       </div>
-      <div className="card-header">
-        <h3>{day}</h3>
-        <div className="temperature">
-          <p>{temp}°</p>
-        </div>
-        {weatherIcon}
-      </div>
-      <div className="card-details">
-        <p>Rain: {rain || '0'} mm</p>
-        <p>Humidity: {humidity}%</p>
-        <p>Wind: {windSpeed} km/h</p>
-        <p>Cloud Cover: {cloudCover}%</p>
-      </div>
+      
+      <ul>
+        <li>Weather: {weather}</li>
+        <li>Temperature: {temperature}°C</li>
+        <li>Feels like: {feelsLike}°C</li>
+        <li>Humidity: {humidity}%</li>
+        <li>Wind: {wind} m/h</li>
+        <li>Pressure: {pressure} hPa</li>
+      </ul>
     </div>
   );
 }
 
 const Weather = () => {
-  const [weekData] = useContext(WeekContext);
 
-  if (!weekData || !weekData.list || weekData.list.length === 0) {
-    return <div className="content">Loading data or no data available...</div>;
+  const [weatherData, setWeatherData] = useContext(DayContext);
+  const [weekData, setWeekData] = useContext(WeekContext);
+
+  let icon = ["Rain", "Rain", "Rain", "Rain", "Rain", "Rain"];
+  let weather = ["Rain", "Rain", "Rain", "Rain", "Rain", "Rain"];
+  let temperature = ["20", "20", "20", "20", "20", "20"];
+  let feelsLike = ["20", "20", "20", "20", "20", "20"];
+  let humidity = ["80", "80", "80", "80", "80", "80"];
+  let wind = ["10", "10", "10", "10", "10", "10"];
+  let pressure = ["12", "12", "12", "12", "12", "12"];
+
+  if (weatherData != null) {
+    icon[0] = weatherData.weather[0].main;
+    weather[0] = weatherData.weather[0].description;
+    temperature[0] = Math.round(weatherData.main.temp);
+    feelsLike[0] = weatherData.main.feels_like;
+    humidity[0] = weatherData.main.humidity;
+    wind[0] = weatherData.wind.speed;
+    pressure[0] = weatherData.main.pressure;
   }
 
-  const todayIndex = new Date().getDay(); 
-  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  if (weekData != null) {
+    for (let i = 1; i < 6; i++) {
+      icon[i] = weekData.list[i].weather[0].main;
+      weather[i] = weekData.list[i].weather[0].description;
+      temperature[i] = Math.round(weekData.list[i].main.temp);
+      feelsLike[i] = weekData.list[i].main.feels_like;
+      humidity[i] = weekData.list[i].main.humidity;
+      wind[i] = weekData.list[i].wind.speed;
+      pressure[i] = weekData.list[i].main.pressure;
+    }
+  }
 
-  const cards = weekData.list.slice(0, 5).map((data, index) => {
-    const dayOfWeek = daysOfWeek[(todayIndex + index) % 7];
-    const weather = data.weather[0].main;
-    const temp = Math.round(data.main.temp);
-
-    const rain = data.rain ? data.rain['3h'] : '0'; // Rain volume for the last 3 hours
-    const humidity = data.main.humidity; // Humidity, %
-    const windSpeed = (data.wind.speed * 3.6).toFixed(2); // Wind speed, converted to km/h and rounded to 2 decimal places
-    const cloudCover = data.clouds.all; // Cloudiness, %
-
-    return (
-      <Card
-        key={index}
-        day={index === 0 ? 'Today' : dayOfWeek}
-        weather={weather}
-        temp={temp}
-        rain={rain}
-        humidity={humidity}
-        windSpeed={windSpeed}
-        cloudCover={cloudCover}
-      />
-    );
-  });
+  // Get the next 5 days
+  var now = new Date();
+  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  var day = [days[now.getDay() + 1], days[now.getDay() + 2], days[now.getDay() + 3], days[now.getDay() + 4], days[now.getDay() + 5]];
 
   return (
-    <div className="content">
-      <div id="activities">
-        <h1>Days</h1>
-        <div id="card-array">{cards}</div>
+    <div id="weather-page">
+      <h1>Weather Overview</h1>
+      <div className="weather-container">
+        <WeatherItem day={"Today"} weather={weather[0]} temperature={temperature[0]} feelsLike={feelsLike[0]} humidity={humidity[0]} wind={wind[0]} pressure={pressure[0]} icon={icon[0]} />
+        <WeatherItem day={day[0]} weather={weather[1]} temperature={temperature[1]} feelsLike={feelsLike[1]} humidity={humidity[1]} wind={wind[1]} pressure={pressure[1]} icon={icon[1]} />
+        <WeatherItem day={day[1]} weather={weather[2]} temperature={temperature[2]} feelsLike={feelsLike[2]} humidity={humidity[2]} wind={wind[2]} pressure={pressure[2]} icon={icon[2]} />
+        <WeatherItem day={day[2]} weather={weather[3]} temperature={temperature[3]} feelsLike={feelsLike[3]} humidity={humidity[3]} wind={wind[3]} pressure={pressure[3]} icon={icon[3]} />
+        <WeatherItem day={day[3]} weather={weather[4]} temperature={temperature[4]} feelsLike={feelsLike[4]} humidity={humidity[4]} wind={wind[4]} pressure={pressure[4]} icon={icon[4]} />
+        <WeatherItem day={day[4]} weather={weather[5]} temperature={temperature[5]} feelsLike={feelsLike[5]} humidity={humidity[5]} wind={wind[5]} pressure={pressure[5]} icon={icon[5]} />
       </div>
       <Background />
     </div>
   );
 };
-
 export default Weather;
