545b11c6e19356725fe031c1d81a1967ca7e989b
ec22761 <ec22761@qmul.ac.uk>
2024-03-17 11:18:56 +0000

    Refactor mobile footer styles to implement animations.

--- src/components/pages/Mobile.js
+++ src/components/pages/Mobile.js
@@ -172,12 +172,14 @@ function Footer() {
     setSelected(!selected);
   };
 
-  if (selected) {
+  let className = selected ? 'big-footer' : 'small-footer';
+  let svg = selected ? 'high' : 'low';
+
     return (
-      <footer className='big-footer'>
-        <MobileWeek />
+      <footer className={className}>
+        {selected ? <MobileWeek /> : null}
         <a href='#' onClick={handleClick}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="78" height="6" viewBox="0 0 78 6" fill="none">
+          <svg className={svg} id='mobile-bar' xmlns="http://www.w3.org/2000/svg" width="78" height="6" viewBox="0 0 78 6" fill="none">
             <path d="M3.10254 3L74.8974 3.00001" stroke="url(#paint0_linear_59_2057)" stroke-width="6" stroke-linecap="round" />
             <defs>
               <linearGradient id="paint0_linear_59_2057" x1="39" y1="3" x2="39" y2="4" gradientUnits="userSpaceOnUse">
@@ -189,23 +191,6 @@ function Footer() {
         </a>
       </footer>
     );
-  }
-
-  return (
-    <footer>
-      <a href='#' onClick={handleClick}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="78" height="6" viewBox="0 0 78 6" fill="none">
-          <path d="M3.10254 3L74.8974 3.00001" stroke="url(#paint0_linear_59_2057)" stroke-width="6" stroke-linecap="round" />
-          <defs>
-            <linearGradient id="paint0_linear_59_2057" x1="39" y1="3" x2="39" y2="4" gradientUnits="userSpaceOnUse">
-              <stop stop-color="#07FFFF" />
-              <stop stop-color="#4CE0E0" />
-            </linearGradient>
-          </defs>
-        </svg>
-      </a>
-    </footer>
-  );
 }
 
 const Mobile = () => {
